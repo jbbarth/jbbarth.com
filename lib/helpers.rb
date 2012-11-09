a79337576fd146869c02af843c94e0fe3c72c583
jbbarth <jeanbaptiste.barth@gmail.com>
2012-11-09 22:01:19 +0100

    First import from old blog

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -5,6 +5,25 @@ include Nanoc3::Helpers::LinkTo
 #ours
 require 'json'
 module NanocHelpers
+  #these helpers are *stupid* and I know that
+  #but for now they work, and I couldn't figure out how to do that
+  #correctly with nanoc classes...
+  def stupid_render(path)
+    html = ""
+    title = YAML.load_file(path)["title"]
+    date = Date.parse path.scan(/\/(\d{4}-\d{2}-\d{2})/).first.first
+    %(#{date.strftime("%d %b %Y")} <a href="#{path.gsub(%r{^./content},'')})
+    html << File.read(path).split("---\n")[2..-1].join("---\n")
+  end
+
+  def stupid_list_links(paths)
+    paths.map do |path|
+      title = YAML.load_file(path)["title"]
+      date = Date.parse path.scan(/\/(\d{4}-\d{2}-\d{2})/).first.first
+      %(<span class="time">#{date.strftime("%d %b %Y")}</span> <a href="#{path.gsub(%r{^./content},'')}">#{title}</a>) if title
+    end.compact.join('<br>')
+  end
+
   def json_block(&block)
     raise "Gimme a block" unless block_given?
     html = json_pretty(capture(&block).strip)
@@ -15,6 +34,9 @@ module NanocHelpers
 
   def json_pretty(json_string)
     JSON.pretty_generate(JSON.parse(json_string))
+  end
+
+  def buffer
   end
 end
 include NanocHelpers
