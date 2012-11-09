c81280cd1e098752cf61fe1d869c1ded77f94483
jbbarth <jeanbaptiste.barth@gmail.com>
2012-11-09 19:30:17 +0100

    Basic layout, styles and dummy content for now

---
+++ lib/helpers.rb
@@ -0,0 +1,20 @@
+#some useful nanoc helpers
+include Nanoc3::Helpers::Rendering
+include Nanoc3::Helpers::LinkTo
+
+#ours
+require 'json'
+module NanocHelpers
+  def json_block(&block)
+    raise "Gimme a block" unless block_given?
+    html = json_pretty(capture(&block).strip)
+    html = %(<pre><code data-language="javascript">)+html+%(</code></pre>)
+    buffer = eval('_erbout', block.binding)
+    buffer << html
+  end
+
+  def json_pretty(json_string)
+    JSON.pretty_generate(JSON.parse(json_string))
+  end
+end
+include NanocHelpers
---
+++ static/javascripts/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  //syntax highlighting
+  function showHiddenParagraphs() {
+    $("p.hidden").fadeIn(500);
+  }
+  setTimeout(showHiddenParagraphs, 1000);
+});
